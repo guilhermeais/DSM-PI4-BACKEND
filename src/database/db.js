49855962803f75140e1e7ee69fe724a87cded151
Guilherme <guilherme.amf18@gmail.com>
2023-04-27 12:40:16 -0300

    feat: test conexão bd azure

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const fs = require("fs")
 
 const sequelize = new Sequelize(`${process.env.DATABASE}`, `${process.env.DBUSER}`, `${process.env.PASSWORD}`, {
-  host: 'localhost',
-  dialect: 'mysql'
+  host: 'aenergy.mysql.database.azure.com',
+  dialect: 'mysql',
+  port: 3306,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // use this if you don't have a CA or self-signed certificate
+      ca: `${process.env.CA}` // use this if you have a CA certificate
+      // cert: fs.readFileSync('/path/to/client.crt'), // use this if you have a client certificate
+      // key: fs.readFileSync('/path/to/client.key') // use this if you have a client key
+    }
+  }
 });
 
 try {
