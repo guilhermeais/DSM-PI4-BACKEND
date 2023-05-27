3881f45f0c14bc6cdba54c08bf6b355c4d073b84
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-05-27 13:11:25 -0300

    test(Products): garante que a media e moda será retornada corretamente

---
+++ test/v1/products/usecases/mount-consumption-details.spec.js
@@ -0,0 +1,50 @@
+import { beforeEach, describe, expect, test } from 'vitest'
+import { MountConsumptionDetails } from '../../../../src/v1/products/domain/usecases/mount-consumption-details'
+
+describe('MountConsumptinoDetails', () => {
+  /**
+   * @type {MountConsumptionDetails}
+   */
+  let sut
+
+  function makeConsumptions() {
+    return {
+      consumptionsInKw: [2, 2],
+      consumptionsInMoney: [10, 10],
+    }
+  }
+
+  beforeEach(() => {
+    sut = new MountConsumptionDetails()
+  })
+
+  test('should return the correct averages', () => {
+    const {
+      consumptionsInKw: { average: averageKw },
+      consumptionsInMoney: { average: averageMoney },
+    } = sut.execute(makeConsumptions())
+
+    const expectedMoneyAverage = 10
+    const expectedKwmAverage = 2
+
+    expect(averageKw).toEqual(expectedKwmAverage)
+    expect(averageMoney).toEqual(expectedMoneyAverage)
+  })
+
+  test('should return the correct modes', () => {
+    const consumptions = {
+      consumptionsInKw: [2, 2, 3, 5, 6, 8, 8],
+      consumptionsInMoney: [10, 10, 1, 2, 5, 5],
+    }
+    const {
+      consumptionsInKw: { mode: kwModes },
+      consumptionsInMoney: { mode: moneyModes },
+    } = sut.execute(consumptions)
+
+    const expectedMoneyModes = [2, 8]
+    const expectedKwmModes = [10, 5]
+
+    expect(kwModes).toEqual(expectedMoneyModes)
+    expect(moneyModes).toEqual(expectedKwmModes)
+  })
+})
