524fff1f7f39dc500d1eef9c7056afeab25b8529
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-06-08 14:40:20 -0300

    test(products): garante que o pubsub irá publicar e ouvir os consumos

---
+++ test/v1/products/infra/gateways/product-pubsub.gateway.test.js
@@ -0,0 +1,30 @@
+import { ProductPubSubEventEmitter } from '../../../../../src/v1/products/infra/gateways/product-pubsub.gateway'
+import { faker } from '@faker-js/faker'
+import { mockConsumption } from '../../../__mocks__/consumption.mock'
+
+describe('ProductPubSubEventEmitter', () => {
+  /**
+   * @type {ProductPubSubEventEmitter}
+   */
+  let sut
+
+  beforeEach(() => {
+    sut = new ProductPubSubEventEmitter()
+  })
+
+  test('should publish an consumption to all listeners', async () => {
+    const mockedProductId = faker.datatype.uuid()
+    const otherMockedProductId = faker.datatype.uuid()
+    const mockedConsumption = mockConsumption()
+    sut.listenToConsumptions(mockedProductId, consumption => {
+      expect(consumption).toEqual(mockedConsumption)
+    })
+
+    sut.listenToConsumptions(otherMockedProductId, consumption => {
+      expect(consumption).toEqual(mockedConsumption)
+    })
+
+    sut.publishConsumption(mockedProductId, mockedConsumption)
+    sut.publishConsumption(otherMockedProductId, mockedConsumption)
+  })
+})
