8a61594834133eabb7fe0e83178960f138917f28
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-06-10 14:11:39 -0300

    test(Math): adiciona método para calcular desvio padrão no MathHelper

--- test/shared/utils/math.spec.js
+++ test/shared/utils/math.spec.js
@@ -1,13 +1,37 @@
-import { describe, expect, test } from 'vitest';
-import { MathHelper } from '../../../src/v1/shared/utils/math';
+import { describe, expect, test } from 'vitest'
+import { MathHelper } from '../../../src/v1/shared/utils/math'
 
 describe('MathHelper', () => {
-  test('should return the mode of an number array', () => {
-    const numbers = [1, 1, 2, 3, 4, 5, 2]
-    const expectedMode = [1,2]
+  describe('getMode()', () => {
+    test('should return the mode of an number array', () => {
+      const numbers = [1, 1, 2, 3, 4, 5, 2]
+      const expectedMode = [1, 2]
 
-    const modes = MathHelper.getMode(numbers)
+      const modes = MathHelper.getMode(numbers)
 
-    expect(modes).toEqual(expectedMode)
-  });
-});
+      expect(modes).toEqual(expectedMode)
+    })
+  })
+
+  describe('getAverage()', () => {
+    test('should return the average of an array of numbers', () => {
+      const numbers = [2, 4]
+      const expectedAverage = 3
+
+      const result = MathHelper.getAverage(numbers)
+
+      expect(result).toEqual(expectedAverage)
+    })
+  })
+
+  describe('getStandardDesviation()', () => {
+    test('should be able to return the standar desviation of an array fo numbers', () => {
+      const numbers = [2, 4, 6, 8, 10]
+      const expectedStandardDesviation = 2.83
+
+      const result = MathHelper.getStandardDeviation(numbers)
+
+      expect(result).toEqual(expectedStandardDesviation)
+    })
+  })
+})
