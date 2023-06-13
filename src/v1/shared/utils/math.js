a4f8de97006809c314f0b242039cbb68b2ca2d96
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-06-12 21:15:14 -0300

    feat(math): adiciona opção para ignorar zeros da media

--- src/v1/shared/utils/math.js
+++ src/v1/shared/utils/math.js
@@ -20,25 +20,30 @@ export class MathHelper {
     return modes
   }
 
-  static getAverage(numbers = []) {
+  static getAverage(numbers = [], { ignoreFalsyNumbers = false } = {}) {
     const sum = numbers.reduce(
       (accumulator, element) => accumulator + element,
       0
     )
+
+    if (ignoreFalsyNumbers) {
+      return sum / numbers.filter(number => number > 0).length
+    }
+
     return sum / numbers.length
   }
 
   static getStandardDeviation(numbers = []) {
-    const average = MathHelper.getAverage(numbers);
-    
+    const average = MathHelper.getAverage(numbers)
+
     const squaredDifferencesSum = numbers.reduce((accumulator, element) => {
-      const difference = element - average;
-      return accumulator + Math.pow(difference, 2);
-    }, 0);
-    
-    const variance = squaredDifferencesSum / numbers.length;
-    const standardDeviation = Math.sqrt(variance);
-  
-    return Number(standardDeviation.toFixed(2));
+      const difference = element - average
+      return accumulator + Math.pow(difference, 2)
+    }, 0)
+
+    const variance = squaredDifferencesSum / numbers.length
+    const standardDeviation = Math.sqrt(variance)
+
+    return Number(standardDeviation.toFixed(2))
   }
 }
